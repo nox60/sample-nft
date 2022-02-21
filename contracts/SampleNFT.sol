7b9de574737a53f416b5feb5a756e731ab18174a
Liu Li <nox60@hotmail.com>
2022-02-21 23:27:52 +0800

    Create SampleNFT.sol

---
+++ contracts/SampleNFT.sol
@@ -0,0 +1,15 @@
+//Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+
+
+contract SampleNFT is ERC721URIStorage, Ownable {
+
+}
+
